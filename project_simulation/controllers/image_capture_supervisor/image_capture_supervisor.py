2e0b9250a4305cbdddb74f16fcfde1a1086b9bcd
Eghosa-Osayande <osayandeeghosam@gmail.com>
2022-12-30 21:21:55 +0100

    feat: done modelling chassis

---
+++ project_simulation/controllers/image_capture_supervisor/image_capture_supervisor.py
@@ -0,0 +1,17 @@
+"""image_capture_supervisor controller."""
+
+
+
+# from controller import Robot, Supervisor
+
+
+# sup = Supervisor()
+
+# robot = sup.getFromDef('ROBOT')
+# translation_field = robot.getField('translation')
+# translation_field.setSFVec3f([-0.8,translation_field.getSFVec3f()[1],translation_field.getSFVec3f()[2]])
+
+
+# timestep = int(sup.getBasicTimeStep())
+# while sup.step(timestep) != -1:
+    # pass
--- project_simulation/controllers/robot_controller/robot_controller.py
+++ project_simulation/controllers/robot_controller/robot_controller.py
@@ -8,36 +8,48 @@ import cv2
 
 robot = Robot()
 
-r1= robot.getDevice('wheelr1')
-r2= robot.getDevice('wheelr2')
-l1= robot.getDevice('wheell1')
-l2= robot.getDevice('wheell2')
+r1= robot.getDevice('wheel_right1')
+r2= robot.getDevice('wheel_right2')
+l1= robot.getDevice('wheel_left1')
+l2= robot.getDevice('wheel_left2')
+sweeper= robot.getDevice('sweeper_motor')
 
-camera = robot.getDevice('camera')
-camera.enable(100)
+# camera = robot.getDevice('camera')
+# camera.enable(100)
 
-
+slab_linear = robot.getDevice('slab_linear_motor')
+slat_rotational= robot.getDevice('slab_rotational_motor')
+slab_linear.setPosition(0.0)
+slab_linear.setVelocity(0.1)
+# slat_rotational.setPosition(-(22/7)/2)
+# slat_rotational.setVelocity(0.5)
 
 
 timestep = int(robot.getBasicTimeStep())
-
-
 while robot.step(timestep) != -1:
+    sweeper_speed=0
+    left=0
+    right=left
+    # slab_linear.setPosition(0.8)
+    # slab_linear.setVelocity(0.1)
+    sweeper.setPosition(float('inf'))
+    sweeper.setVelocity(sweeper_speed)
+    
     r1.setPosition(float('inf'))
-    r1.setVelocity(1)
+    r1.setVelocity(right)
     
     r2.setPosition(float('inf'))
-    r2.setVelocity(1)
+    r2.setVelocity(right)
     
     l1.setPosition(float('inf'))
-    l1.setVelocity(1)
+    l1.setVelocity(left)
     
     l2.setPosition(float('inf'))
-    l2.setVelocity(1)
+    l2.setVelocity(left)
     
-    cameraData=camera.getImage()
-    image = np.frombuffer(cameraData, np.uint8).reshape((camera.getHeight(), camera.getWidth(), 4))
+    # cameraData=camera.getImage()
+    # image = np.frombuffer(cameraData, np.uint8).reshape((camera.getHeight(), camera.getWidth(), 4))
     
-    pass
+    # pass
 
 
